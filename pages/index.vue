7a95bcb8946f6dc782775d5fb9c5156c93a0227b
Andy <andy@Andys-MacBook-Pro.local>
2022-06-23 21:27:11 -0400

    fetch

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,28 @@
 <script setup>
+const { data } = await useFetch('/api/tickers?limit=10')
 </script>
 
 <template>
+  <main>
+    <h1>Index Page</h1>
+
+    <table border="1 px solid">
+      <thead>
+      <tr>
+        <th>Name</th>
+        <th>Symbol</th>
+        <th>Price</th>
+        <th>Details</th>
+      </tr>
+      </thead>
+      <tr v-for="currency in data.data" :key="data.data.id">
+        <td>{{ currency.name }}</td>
+        <td>{{ currency.symbol }}</td>
+        <td>{{ currency.price_usd }}</td>
+        <td>
+          <NuxtLink :to="'/currency/' + currency.id">{{ currency.id }}</NuxtLink>
+        </td>
+      </tr>
+    </table>
+  </main>
 </template>
